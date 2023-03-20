f263135528f1ebb5d5b63deca80ba4260d7dab71
Anushka Ashok Dhotre <anushkadhotre009@gmail.com>
2023-03-20 20:47:41 +0530

    commit

--- vue/calculator/src/App.vue
+++ vue/calculator/src/App.vue
@@ -12,6 +12,8 @@ export default {
   components: {
     calculator
     
+
+    
   }
 }
 </script>
--- vue/calculator/src/main.js
+++ vue/calculator/src/main.js
@@ -5,6 +5,7 @@ import  'bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false;
 
 
+
 new Vue({
     render:h =>h(App),
 }).$mount('#app')
